3006ccf0ee634fdbaaeaeda1bea1b01b008b0f69
Park Hyeonggyu <162649328+Tom7418@users.noreply.github.com>
2024-03-26 13:44:12 +0900

    Update mycalculator(switch).java

--- mycalculator(switch).java
+++ mycalculator(switch).java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class mycalculator(switch){
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.print("계산식 => ")
+    int num1 = scanner.nextInt();
+    String sign = scanner.next();
+    int num2 = scanner.nextInt();
+    String Str = num1 + sing + num2;
+  
+  }
+  
+}
